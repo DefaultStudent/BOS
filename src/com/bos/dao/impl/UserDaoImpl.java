617f6e13d8dd6ba064ee95c8277f27cc888688ae
DefaultStudent <comrade_vodka@outlook.com>
2019-01-08 11:30:05 +0800

    数据库调试

--- src/com/bos/dao/impl/UserDaoImpl.java
+++ src/com/bos/dao/impl/UserDaoImpl.java
@@ -14,7 +14,7 @@ import java.util.List;
 public class UserDaoImpl extends BaseDaoImpl<User> implements IUserDao{
     @Override
     public User findByUsernameAndPassword(String username, String password) {
-        String hql = "FROM User u where u.username = ? and password = ?";
+        String hql = "from User u where u.username = ? and u.password = ?";
         List<User> list = (List<User>) this.getHibernateTemplate().find(hql, username, password);
         if (list != null && list.size() > 0) {
             return list.get(0);
